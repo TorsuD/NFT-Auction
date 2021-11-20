2f0838bf4bb399d394581c3d7a50f775900f331c
Derrick Torsu <torsuderrick2004@gmail.com>
2021-11-20 19:59:02 +0000

    Added a view

--- screens/Search.js
+++ screens/Search.js
@@ -207,7 +207,7 @@ export default function Search() {
           <View style={{ flexDirection: "row" }}></View>
         </ScrollView>
         <View>
-          <Text>You can code here</Text>
+          <Text>You can code right?</Text>
         </View>
       </ScrollView>
     </View>
