c5b91b1ed442c782e1f6b477238d12e33260f95c
Derrick Torsu <torsuderrick2004@gmail.com>
2021-12-21 20:58:21 +0000

    Created a component to handle arts on preview

---
+++ components/LatestArt.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const LatestArt = () => {
+  return (
+    <View>
+      <Text>latestArt</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default LatestArt;
