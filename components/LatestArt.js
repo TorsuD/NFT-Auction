c0e3400cd802ba3268297a75510ba526a7021dd5
Derrick Torsu <torsuderrick2004@gmail.com>
2021-12-21 21:23:26 +0000

    Added an image to the latest art

--- components/LatestArt.js
+++ components/LatestArt.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-const LatestArt = () => {
+const LatestArt = (props) => {
   return (
-    <View>
-      <Text>latestArt</Text>
+    <View style={styles.container}>
+      <Image source={require("../assets/la.jpg")} style={styles.image} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 170,
+    width: 170,
+    position: "relative",
+    borderRadius: 40,
+  },
+  container: {
+    alignContent: "center",
+    position: "relative",
+    marginLeft: 20,
+  },
+});
 
 export default LatestArt;
