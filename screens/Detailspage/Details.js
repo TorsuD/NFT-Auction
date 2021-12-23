a7720e32d5d1c42589eb7a41dd2b4e6acba28eef
Derrick Torsu <torsuderrick2004@gmail.com>
2021-12-23 01:51:04 +0000

    Details of an NFT Art available for bidding

--- screens/Detailspage/Details.js
+++ screens/Detailspage/Details.js
@@ -1,10 +1,100 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import style, { styles } from "../Detailspage/style";
+import { AntDesign } from "@expo/vector-icons";
+import { EvilIcons } from "@expo/vector-icons";
+import { SimpleLineIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function Details() {
   return (
-    <View style={{ marginTop: 100 }}>
-      <Text>Details</Text>
+    //Upper Container
+    <View style={styles.container}>
+      <View style={styles.upperContainer}>
+        <View style={styles.header}>
+          <AntDesign name="left" size={24} color="white" style={styles.space} />
+          <Text style={styles.headerDetail}>Details</Text>
+          <AntDesign
+            name="hearto"
+            size={24}
+            color="white"
+            style={styles.space}
+          />
+        </View>
+
+        {/**The Image */}
+        <View style={styles.imageViewer}>
+          <Image source={require("../../assets/cm.jpg")} style={styles.image} />
+        </View>
+      </View>
+
+      {/**Lower Container */}
+      {/**The 1st Lower Header */}
+      <View style={styles.lowerContainer}>
+        <View style={styles.lowerHeader}>
+          <Text style={styles.title}>Monkey Fever</Text>
+
+          <View style={styles.lowerHeaderRight}>
+            <EvilIcons
+              name="share-apple"
+              size={32}
+              color="white"
+              style={styles.space}
+            />
+            <AntDesign
+              name="sharealt"
+              size={24}
+              color="white"
+              style={styles.space}
+            />
+            <SimpleLineIcons
+              name="options-vertical"
+              size={24}
+              color="white"
+              style={styles.space}
+            />
+          </View>
+        </View>
+        {/**The 2nd Lower Header With Verification */}
+        <View style={styles.verification}>
+          <Text style={styles.verificationTitle}>ABSTRACT World</Text>
+          <MaterialIcons
+            name="verified"
+            size={20}
+            color="#03a9fc"
+            style={{ marginLeft: 3 }}
+          />
+        </View>
+
+        {/** Art Description  */}
+        <View>
+          <Text style={styles.description}>
+            Welcome to the abstract world of NFT.Unforgettable patterns and
+            color combinations will remain in your head for a long time. Power
+            is in the beauty!{" "}
+          </Text>
+        </View>
+
+        {/**The Bidding Option And Highest Bidding */}
+        <View style={styles.bidContainer}>
+          <View>
+            <Text style={styles.bid}>Highest Bid</Text>
+            <Text style={styles.bidNumber}>140</Text>
+          </View>
+
+          {/**Button */}
+          <TouchableOpacity
+            onPress={() => {
+              alert("Nice bid for this NFT Art! ♥️ Explore more wonders!");
+            }}
+          >
+            <View style={styles.bidButton}>
+              <Text style={styles.placeABid}>Place a bid</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
